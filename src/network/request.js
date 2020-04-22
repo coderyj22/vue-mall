f83d79644c4b482b745754f6aac408291ab57e77
coderyj22 <1052670316@qq.com>
2020-04-22 17:17:17 +0800

    .

--- src/network/request.js
+++ src/network/request.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export function request(config) {
   const instance = axios.create({
     // baseURL:'http://123.207.32.32:8000/api/...',
-    baseURL: 'http://106.54.54.237:8000/api/...',   
+    baseURL: 'http://106.54.54.237:8000/api/h8',   
     timeout: 10000
   })
   return instance(config)
